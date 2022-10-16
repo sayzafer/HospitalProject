646ea9820561d1448e6a3df85a128227b666ee5c
Zafer Say <zafersay93@gmail.com>
2022-10-16 19:39:55 +0300

    db connect

---
+++ HospitalProject/sqlbaglantisi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace HospitalProject
+{
+    internal class sqlbaglantisi
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection connect = new SqlConnection("Data Source=DESKTOP-UMTLPQ5;Initial Catalog=HospitalProject;Integrated Security=True");
+            connect.Open();
+            return connect;
+        }
+    }
+}
